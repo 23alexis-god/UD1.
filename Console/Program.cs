2ba6f3ad4953b4d821d0c859a691695219f85cf4
gaiua <gaiua@ALEXIS23>
2021-06-18 13:30:28 +0200

    Agregar archivos de proyecto.

---
+++ Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int num1 = 5;
+            int num2 = 6;
+            double numero3 = 2,57;
+            double = suma;
+            suma = num1 + numero3;
+
+            Console.Write.Line(suma);
+            string valor1 = Console.ReadLine();
+            Console.Write.Line("El numero introducido es: " + valor1)
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+    }
+}
+
